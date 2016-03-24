65ca342589609aad98a509b5a84c766c562e1da2
peter2774163 <peter2774163@yahoo.com.tw>
2016-03-24 09:25:04 +0800

    change

--- src/Square.java
+++ src/Square.java
@@ -2,34 +2,86 @@ import java.awt.Color;
 import java.util.Random;
 
 public class Square extends Shape {
-	
+
 	private int shapeWidth;
-	
+
 	public Square(int shapeWidth){
 		// TODO Constructor
 		this.shapeWidth = shapeWidth;
 	}
-	
-	
+
+
 	@Override
-	public Color getRandomColor() {		
+	public Color getRandomColor() {
 		// TODO Return random color produce from java.util.Random;
 		// Java 'Color' class takes 3 floats, from 0 to 1.
-		
+
 		// TODO Return color produced by three rgb floats.
 		return new Color(r, g, b);
 	}
-	
+
 	@Override
 	public int getShapeWidth(){
 		// TODO Return Square width
 		return 0;
 	}
-	
+
 	@Override
 	public int getShapeHeight(){
 		// TODO Return Square height
 		return 0;
+	}
+
+
+	@Override
+	public int getCenterX(int mouseX) {
+		// TODO Input mouse X position and return center X of square
+		return mouseX - shapeWidth/2;
+	}
+
+
+	@Override
+	public int getCenterY(int mouseY) {
+		// TODO Input mouse Y position and return center Y of square
+		return mouseY - shapeWidth/2;
+	}
+
+
+}import java.awt.Color;
+import java.util.Random;
+
+public class Square extends Shape {
+
+	private int shapeWidth;
+
+	public Square(int shapeWidth){
+		// TODO Constructor
+		this.shapeWidth = shapeWidth;
+	}
+
+
+	@Override
+	public Color getRandomColor() {
+		// TODO Return random color produce from java.util.Random;
+		// Java 'Color' class takes 3 floats, from 0 to 1.
+		Random rand = new Random();
+		float r = rand.nextFloat();
+		float g = rand.nextFloat();
+		float b = rand.nextFloat();
+		// TODO Return color produced by three rgb floats.
+		return new Color(r, g, b);
+	}
+
+	@Override
+	public int getShapeWidth(){
+		// TODO Return Square width
+		return this.shapeWidth;
+	}
+
+	@Override
+	public int getShapeHeight(){
+		// TODO Return Square height
+		return this.shapeWidth;
 	}
 
 
